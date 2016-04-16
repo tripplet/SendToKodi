1cde31b6b20824547f2caeeff0bf45d5d243c5c8
Tobias Tangemann <tobiastangemann@gmail.com>
2016-04-16 11:16:28 +0200

    Small cleanup

--- SendToKodi/AppDelegate.swift
+++ SendToKodi/AppDelegate.swift
@@ -14,13 +14,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     let mirroredDefaults = NSUserDefaults(suiteName: USER_DEFAULTS_SUITE)!
 
-    // Save all user default into app group user defaults, so they can be accessed from the extension
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleUserDefaultsChanged:", name: NSUserDefaultsDidChangeNotification, object: NSUserDefaults.standardUserDefaults())
+        NSApplication.sharedApplication().servicesProvider = self
+        
+        // Save all user default into app group user defaults, so they can be accessed from the extension
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(AppDelegate.handleUserDefaultsChanged(_:)),
+                                                                   name: NSUserDefaultsDidChangeNotification,
+                                                                 object: NSUserDefaults.standardUserDefaults())
     }
 
     func handleUserDefaultsChanged(aNotification: NSNotification) {
         mirroredDefaults.setValue(NSUserDefaults.standardUserDefaults().stringForKey("kodi_hostname")!, forKey: "kodi_hostname")
         mirroredDefaults.synchronize()
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
     }
 }
